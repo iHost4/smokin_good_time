d26df1ff4c2a48a359554f90861e6f3c6e299e39
ZACHAPSUACCOUNT <ZMcDonald1@my.apsu.edu>
2025-04-25 09:31:46 -0500

    Add files via upload
    Uploaded files contain menu item descriptions, a cart system that allows the used to add to a cart and a button on the main page that says "send to restaurant" this button does not do anything because I'm not sure how you get this talking to the backend work you've done.

---
+++ send away on4-25-2-25/index.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap eCommerce Page Template</title>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap-4.4.1.css" rel="stylesheet">
+	  <link rel="stylesheet" href="css/zach.css">
+  </head>
+  <body>
+    
+<hr>
+    <h2 class="text-center">OUR MENU</h2>
+    <hr>
+    <div class="container">
+      <div class="row text-center">
+		  
+		  
+		  
+		  
+        <div class="col-md-4 pb-1 pb-md-0">
+			
+			<a href="./appetizers/appetizers.html">
+			
+          <div class="card">
+            <img class="card-img-top" src="images/ZachImages/Appetizers.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Appetizers&nbsp;</h5>
+              <p class="card-text">Start your BBQ journey off right with our crave-worthy starters. These Southern classics are the perfect way to whet your appetite and bring the heat before the main event.</p>
+				
+				
+              
+            </div>
+          </div>
+			
+				</a>
+			
+        </div>
+		  
+		  
+		  
+		  
+        <div class="col-md-4 pb-1 pb-md-0">
+			
+			<a href="./entree/entree.html">
+			
+          <div class="card">
+            <img class="card-img-top" src="images/ZachImages/Entrees.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Entrees&nbsp;</h5>
+              <p class="card-text">Smoked low and slow, our entrées are packed with bold BBQ flavor. From tender brisket to saucy ribs and smoky salmon, these main courses are the real stars of the show.</p>
+</div>
+          </div>
+			
+			</a>
+			
+        </div>
+		  
+		  
+        <div class="col-md-4 pb-1 pb-md-0">
+			
+			<a href="./sides/sides.html">
+			
+          <div class="card">
+            <img class="card-img-top" src="images/ZachImages/Sides.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Sides&nbsp;</h5>
+              <p class="card-text">No BBQ plate is complete without the fixin’s. Our homestyle sides are full of flavor-each one crafted to complement our pit-smoked meats perfectly.</p>
+</div>
+          </div>
+       
+		  </a>
+		  
+		  
+		  </div>
+      </div>
+      <div class="row text-center mt-4">
+        <div class="col-md-4 pb-1 pb-md-0">
+			
+			<a href="./kids-menu/kids-menu.html">
+			
+          <div class="card">
+            <img class="card-img-top" src="images/ZachImages/Kids-Menu.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Kids Menu&nbsp;</h5>
+              <p class="card-text">Perfect tasting BBQ isn’t just for the grown-ups! Our kids' menu is packed with mini portions of our smokin’ favorites—perfect for little hands and big appetites.</p>
+</div>
+          </div>
+			
+			</a>
+			
+        </div>
+		  
+		  
+		  
+        <div class="col-md-4 pb-1 pb-md-0">
+			
+			<a href="./desserts/desserts.html">
+			
+          <div class="card">
+            <img class="card-img-top" src="images/ZachImages/Desserts.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Desserts&nbsp;</h5>
+              <p class="card-text">Save room for something sweet! From rich chocolate lava cake to our famous banana pudding, these Southern treats are the perfect way to finish off your BBQ feast.</p>
+</div>
+          </div>
+			
+			</a>
+			
+        </div>
+		  
+		  
+		  
+		  
+        <div class="col-md-4 pb-1 pb-md-0">
+			
+			
+			<a href="./drinks/drinks.html">
+			
+          <div class="card">
+            <img class="card-img-top" src="images/ZachImages/Drinks.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Drinks&nbsp;</h5>
+              <p class="card-text">Wash it all down with a glass of Southern hospitality. Whether you’re sipping sweet tea or cracking open a cold root beer, we’ve got the perfect pour to go with your meal.</p>
+</div> 
+          </div>
+			</a>
+
+        </div>
+      </div>
+    </div>
+    <hr>
+	  
+	  <div class="text-center">
+	  <button class="zachbtn"><h2>Send to Resturant</h2></button>
+	  </div>
+	  
+	  
+    <hr>
+    <div class="container text-white bg-dark p-4">
+      <div class="row">
+        <div class="col-6 col-md-8 col-lg-7">
+          <div class="row text-center">
+            <div class="col-sm-6 col-md-4 col-lg-4 col-12">
+              <ul class="list-unstyled">
+                <li class="btn-link"> <a>Our Suppliers</a> </li>
+                <li class="btn-link"> <a>Our Suppliers</a> </li>
+                <li class="btn-link"> <a>Our Suppliers</a> </li>
+                <li class="btn-link"> <a>Our Suppliers</a> </li>
+                <li class="btn-link"> <a>Our Suppliers</a> </li>
+              </ul>
+            </div>
+            <div class="col-sm-6 col-md-4 col-lg-4 col-12">
+              <ul class="list-unstyled">
+                <li class="btn-link"> <a>Our Suppliers</a> </li>
+                <li class="btn-link"> <a>Our Suppliers</a> </li>
+                <li class="btn-link"> <a>Our Suppliers</a> </li>
+                <li class="btn-link"> <a>Our Suppliers</a> </li>
+                <li class="btn-link"> <a>Our Suppliers</a> </li>
+              </ul>
+            </div>
+            <div class="col-sm-6 col-md-4 col-lg-4 col-12">
+              <ul class="list-unstyled">
+                <li class="btn-link"> <a>Our Suppliers</a> </li>
+                <li class="btn-link"> <a>Our Suppliers</a> </li>
+                <li class="btn-link"> <a>Our Suppliers</a> </li>
+                <li class="btn-link"> <a>Our Suppliers</a> </li>
+                <li class="btn-link"> <a>Our Suppliers</a> </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 col-lg-5 col-6">
+          <address>
+            <strong>Smokin' Good Time, Inc.</strong><br>
+            Fake Dr.<br>
+            Clarksville, TN, 37040-0219<br>
+            Phone: (123) 456-7890
+          </address>
+          <address>
+            <strong>Group 3</strong><br>
+            <a href="mailto:#">fake.fake@fake.com</a>
+          </address>
+        </div>
+      </div>
+    </div>
+    <footer class="text-center">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <p>Copyright © MyFakeWebsite. All rights reserved.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="../js/jquery-3.4.1.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="../js/popper.min.js"></script>
+    <script src="../js/bootstrap-4.4.1.js"></script>
+  </body>
+</html>
